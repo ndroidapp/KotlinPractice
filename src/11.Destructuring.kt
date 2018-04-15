2a8734af6195df8e91e852f0a2a4f6491b14363b
ndroidapp <ndroidapp.in@gmail.com>
2018-04-15 14:24:28 +0530

    Constructor, Inheritance, Interface

--- src/11.Destructuring.kt
+++ src/11.Destructuring.kt
@@ -3,6 +3,7 @@ data class Student(val  a:String, val b:String){
     var subject:String=b
 }
 fun main(args: Array<String>) {
+
     val s=Student("John","Kotlin")
     val(name,subject)=s
     println("Name: $name\nSubject: $subject")
